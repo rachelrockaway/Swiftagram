6e34be69bada220f49d7fa46664a1c9eedf030b4
Rachel <rachel@rachelrockaway.com>
2015-11-24 03:51:54 -0500

    Add Compose View Controller, Compose View Controller UI, Compose View Controller variables and Compose View Controller add image function and compose function.

---
+++ Swiftagram/ComposeViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ComposeViewController.swift
+//  Swiftagram
+//
+//  Created by Rachel Stevenson on 11/24/15.
+//  Copyright © 2015 rachelrockaway. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+import Bolts
+
+class ComposeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var previewImage: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addImageTapped(sender: AnyObject) {
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(.PhotoLibrary)!
+        imagePicker.allowsEditing = false
+        
+        self.presentViewController(imagePicker, animated: true
+            , completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        self.previewImage.image = image
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    @IBAction func composeTapped(sender: AnyObject) {
+        
+        let date = NSDate()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
+        let localDate = dateFormatter.stringFromDate(date)
+        
+        let imageToBeUploaded = self.previewImage.image
+        let imageData = UIImagePNGRepresentation(imageToBeUploaded!)!
+        
+        let file: PFFile = PFFile(data: imageData)!
+        let fileCaption: String = self.captionTextView.text
+        
+        let photoToUpload = PFObject(className: "Posts")
+        photoToUpload["Image"] = file
+        photoToUpload["Caption"] = fileCaption
+        photoToUpload["addedBy"] = PFUser.currentUser()?.username
+        photoToUpload["date"] = localDate
+        
+        do {
+            try photoToUpload.save()
+        }catch {
+            print("Error.")
+        
+    }
+    
+}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
